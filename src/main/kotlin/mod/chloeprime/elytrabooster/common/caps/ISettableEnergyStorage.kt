74ae9362ed907164570d501b50c6489e4e7adbfd
ChloePrime <chloe_von_einzbern@qq.com>
2021-05-24 12:43:57 +0800

    替换原本十分Glitchy的物品电量实现

--- src/main/kotlin/mod/chloeprime/elytrabooster/common/caps/ISettableEnergyStorage.kt
+++ src/main/kotlin/mod/chloeprime/elytrabooster/common/caps/ISettableEnergyStorage.kt
@@ -4,23 +4,14 @@ import net.minecraft.util.math.MathHelper
 import net.minecraftforge.energy.EnergyStorage
 import net.minecraftforge.energy.IEnergyStorage
 
-class SettableEnergyStorage(
-    capacity: Int,
-    maxReceive: Int,
-    maxExtract: Int,
-    energy: Int
-): EnergyStorage(capacity, maxReceive, maxExtract, energy) {
-    constructor(capacity: Int) : this(capacity, capacity, capacity, 0)
-
-    fun setEnergyStored(energyIn: Int) {
-        energy = energyIn
-    }
+interface ISettableEnergyStorage: IEnergyStorage{
+    fun setEnergyStored(value: Int)
 }
 
 var IEnergyStorage.energy: Int
 get() = this.energyStored
 set(value) {
-    if (this is SettableEnergyStorage) {
+    if (this is ISettableEnergyStorage) {
         this.energyStored = MathHelper.clamp(value, 0, this.maxEnergyStored)
         return
     }
--- src/main/kotlin/mod/chloeprime/elytrabooster/common/item/BoostedElytraItem.kt
+++ src/main/kotlin/mod/chloeprime/elytrabooster/common/item/BoostedElytraItem.kt
@@ -2,7 +2,7 @@ package mod.chloeprime.elytrabooster.common.item
 
 import mod.chloeprime.elytrabooster.api.common.IBoostedElytraItem
 import mod.chloeprime.elytrabooster.api.common.IElytraInputCap
-import mod.chloeprime.elytrabooster.common.caps.SettableEnergyStorage
+import mod.chloeprime.elytrabooster.common.caps.ISettableEnergyStorage
 import mod.chloeprime.elytrabooster.common.caps.energy
 import mod.chloeprime.elytrabooster.common.network.ModNetworking
 import mod.chloeprime.elytrabooster.common.network.SEnergyUpdatePacket
@@ -26,10 +26,10 @@ import net.minecraftforge.api.distmarker.OnlyIn
 import net.minecraftforge.common.capabilities.Capability
 import net.minecraftforge.common.capabilities.CapabilityInject
 import net.minecraftforge.common.capabilities.ICapabilityProvider
-import net.minecraftforge.common.capabilities.ICapabilitySerializable
 import net.minecraftforge.common.util.LazyOptional
 import net.minecraftforge.energy.IEnergyStorage
 import net.minecraftforge.fml.network.PacketDistributor
+import kotlin.math.min
 
 /**
  * 需要消耗能源的推进鞘翅
@@ -72,55 +72,66 @@ open class BoostedElytraItem(
             }
             entity.getCapability(INPUT_CAP!!).map { input ->
                 stack.getCapability(ENERGY_CAP!!).map {
-                    extractEnergyAndUpdate(it, costFormula.applyAsInt(input), entity)
+                    it.energy -= costFormula.applyAsInt(input)
                     it.energyStored > 0
                 }.orElse(false)
             }.orElse(true)
         }
     }
 
-    /**
-     * 消耗能源，
-     * 然后向玩家发送网络包以刷新客户端显示
-     */
-    private fun extractEnergyAndUpdate(battery: IEnergyStorage, amount: Int, entity: LivingEntity){
-        val dirty = battery.energyStored > 0 && amount > 0
-        battery.energy -= amount
-        if (dirty && !entity.world.isRemote && entity is ServerPlayerEntity) {
-            ModNetworking.CHANNEL.send(
-                PacketDistributor.PLAYER.with { entity },
-                SEnergyUpdatePacket(SLOT, battery.energyStored)
-            )
-        }
-    }
-
     // 储电
 
     override fun initCapabilities(stack: ItemStack, nbt: CompoundNBT?): ICapabilityProvider? {
-        return CapProvider()
+        return CapProvider(stack)
     }
 
-    private inner class CapProvider : ICapabilitySerializable<IntNBT> {
-        private var instance = createInstance(0)
+    private inner class CapProvider(
+        private val stack: ItemStack
+    ) : ICapabilityProvider, ISettableEnergyStorage {
+        private var instance = LazyOptional.of { this }
 
         override fun <T : Any?> getCapability(cap: Capability<T>, side: Direction?): LazyOptional<T> {
             return ENERGY_CAP!!.orEmpty(cap, instance.cast())
         }
 
-        override fun serializeNBT(): IntNBT {
-            return IntNBT.valueOf(
-                instance.map { it.energyStored }.orElse(0)
-            )
+        override fun getEnergyStored(): Int {
+            val nbt = stack.orCreateTag["fe"]
+            return if (nbt is IntNBT) {
+                nbt.int
+            } else {
+                stack.orCreateTag.putInt("fe", 0)
+                0
+            }
         }
 
-        override fun deserializeNBT(nbt: IntNBT?) {
-            val energyInNbt = nbt?.int ?: return
-            instance = createInstance(energyInNbt)
+        override fun setEnergyStored(value: Int) {
+            stack.orCreateTag.putInt("fe", value)
         }
 
-        private fun createInstance(energy: Int) = LazyOptional.of {
-            SettableEnergyStorage(maxEnergy.asInt, maxEnergy.asInt, 0, energy)
+        override fun receiveEnergy(given: Int, simulate: Boolean): Int {
+            val current = energyStored
+            val actualReceive = min(given, maxEnergyStored - current)
+            if (!simulate) {
+                energyStored = current + actualReceive
+            }
+            return actualReceive
         }
+
+        override fun extractEnergy(request: Int, simulate: Boolean): Int {
+            val current = energyStored
+            val actualExtract = min(request, current)
+            if (!simulate) {
+                energyStored = current - actualExtract
+            }
+            return actualExtract
+        }
+
+        override fun getMaxEnergyStored(): Int = maxEnergy.asInt
+
+        override fun canExtract(): Boolean = false
+
+        override fun canReceive(): Boolean = true
+
     }
 
     // 让耐久条显示电力情况
@@ -130,6 +141,7 @@ open class BoostedElytraItem(
         val dur = 1F - getDurabilityForDisplay(stack).toFloat()
         return MathHelper.hsvToRGB(DURABILITY_BAR_HUE, dur, 0.25F * dur + 0.75F)
     }
+
     override fun getDurabilityForDisplay(stack: ItemStack): Double {
         return stack.getCapability(ENERGY_CAP!!).map {
             1F - it.energyStored.toDouble() / it.maxEnergyStored
@@ -146,7 +158,7 @@ open class BoostedElytraItem(
         val fullEnergyStack = defaultInstance
         ENERGY_CAP?.let { cap ->
             fullEnergyStack.getCapability(cap).ifPresent {
-                it.receiveEnergy(it.maxEnergyStored, false)
+                it.energy = it.maxEnergyStored
                 items.add(fullEnergyStack)
             }
         }
@@ -163,7 +175,7 @@ open class BoostedElytraItem(
             stack.getCapability(ENERGY_CAP!!).ifPresent {
                 tooltip.add(
                     TextFormats.getProgressText(
-                        it.energyStored, it.maxEnergyStored, 0x00FFFF,"FE"
+                        it.energyStored, it.maxEnergyStored, 0x00FFFF, "FE"
                     )
                 )
             }
